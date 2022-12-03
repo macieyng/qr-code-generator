1617a60e255df9dfc3e149a7ac3cc383656d93c0
Maciej Nachtygal <maciejnachtygal@Maciejs-MacBook-Pro.local>
2022-12-03 14:19:38 +0100

    create index file

---
+++ frontend/main.js
@@ -0,0 +1,13 @@
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+
+function convertMsToTime(milliseconds) {
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+
+  seconds = seconds % 60;
+  minutes = minutes % 60;
+
+  return `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`;
+}
