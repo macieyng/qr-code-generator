63046d509ecdda0a4a66e04e33abcb95ccc14df5
Maciej Nachtygal <maciejnachtygal@Maciejs-MacBook-Pro.local>
2022-12-10 20:42:32 +0100

    allow adding new qr codes

--- app/main.py
+++ app/main.py
@@ -6,18 +6,26 @@ from enum import Enum
 from http import HTTPStatus
 from datetime import datetime
 import json
-
+from typing import Self
 import qrcode
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import RedirectResponse, StreamingResponse
 from pydantic import BaseModel as PydanticBaseModel, Field, root_validator
 from pymongo import MongoClient
 from qrcode.image.styledpil import StyledPilImage
-from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
+from qrcode.image.styles import moduledrawers
+from qrcode.image.styles import colormasks
+import time
+
 from fastapi.middleware.cors import CORSMiddleware
+import logging
+
+from functools import partial
 
 
 app = FastAPI()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 app.add_middleware(
     CORSMiddleware,
@@ -42,9 +50,10 @@ class BaseModel(PydanticBaseModel):
         json_encoders = {
             datetime: lambda dt: dt.timestamp(),
         }
-        
+
     def to_document_dict(self):
         return json.loads(self.json())
+
 
 class QRCodeDrawers(str, Enum):
     SQUARE_MODULE = "SQUARE_MODULE"
@@ -65,7 +74,7 @@ class QRCodeMasks(str, Enum):
 
 
 class Color(BaseModel):
-    back: str
+    background: str
     primary: str | None
     secondary: str | None
 
@@ -75,7 +84,9 @@ class CreateQRCodeAPIRequest(BaseModel):
     target_url: str
     drawer: QRCodeDrawers | None
     mask: QRCodeMasks | None
-    color: Color | None
+    primary_color: str | None
+    secondary_color: str | None
+    background_color: str | None
 
 
 class GetQRCodeAPIResponse(BaseModel):
@@ -86,12 +97,21 @@ class GetQRCodeAPIResponse(BaseModel):
     identifier: str
     image: str
     created_at: datetime = Field(default_factory=datetime.utcnow)
-    
+
     @root_validator(pre=True)
     def set_name(cls, values):
         if "name" not in values:
             values["name"] = extract_domain_from_url(values["target_url"])
         return values
+
+
+class GetPreviewAPIResponse(BaseModel):
+    image: str
+
+
+class GetPreviewAPIRequest(BaseModel):
+    drawer: QRCodeDrawers | None
+    mask: QRCodeMasks | None
 
 
 class QRCodeEntryDB(BaseModel):
@@ -101,14 +121,15 @@ class QRCodeEntryDB(BaseModel):
     identifier: str
     image: str
     name: str
-    
-    
+
+
 class PageResponse(BaseModel):
     kind: str = BaseModel.__name__
     items: list[BaseModel]
     page: int
     page_size: int
-    
+    next_page: int | None
+
 
 class ListQRCodeAPIResponse(PageResponse):
     items: list[GetQRCodeAPIResponse]
@@ -118,7 +139,7 @@ class ListQRCodeAPIResponse(PageResponse):
 class ListQrCodeAPIRequset(BaseModel):
     page: int = 1
     page_size: int = 10
-    
+
 
 @app.get("/health")
 async def health():
@@ -129,15 +150,183 @@ def extract_domain_from_url(url):
     return url.split("/")[2]
 
 
+class RGBColor(tuple):
+    def __new__(cls, value):
+        try:
+            return cls.from_hex_color(cls, value)
+        except TypeError:
+            pass
+        if not isinstance(value, tuple):
+            raise TypeError("Expected tuple")
+        if len(value) != 3:
+            raise ValueError("Expected tuple of length 3")
+        return tuple.__new__(cls, value)
+
+    def __repr__(self) -> str:
+        return f"<RGBColor({self[0]}, {self[1]}, {self[2]})>"
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    @classmethod
+    def from_hex_color(cls, hex_color: str) -> Self:
+        return HEXColor(hex_color).to_rgb_color()
+
+
+class HEXColor(str):
+    def __new__(cls, value):
+        if not isinstance(value, str):
+            logger.info(type(value))
+            raise TypeError("Expected string")
+        if not value.startswith("#"):
+            logger.info(value)
+            raise ValueError("Expected string starting with #")
+        if len(value) != 7:
+            logger.info(value)
+            raise ValueError("Expected string of length 7")
+        logger.info(value)
+        return str.__new__(cls, value)
+
+    def __repr__(self) -> str:
+        return f"<HEXColor({self})>"
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    def to_rgb_color(self) -> RGBColor:
+        return RGBColor((int(self[1:3], 16), int(self[3:5], 16), int(self[5:7], 16)))
+
+
+def hex_to_rgb(*args) -> RGBColor | None:
+    hex_color = args[0]
+    logger.info(hex_color)
+    if not hex_color:
+        return None
+    return HEXColor(hex_color).to_rgb_color()
+
+
+def map_drawers(drawer: QRCodeDrawers) -> moduledrawers.QRModuleDrawer:
+    return {
+        QRCodeDrawers.CIRCLE_MODULE: moduledrawers.CircleModuleDrawer(),
+        QRCodeDrawers.GAPPED_SQUARE_MODULE: moduledrawers.GappedSquareModuleDrawer(),
+        QRCodeDrawers.HORIZONTAL_BARS: moduledrawers.HorizontalBarsDrawer(),
+        QRCodeDrawers.ROUNDED_MODULE: moduledrawers.RoundedModuleDrawer(),
+        QRCodeDrawers.SQUARE_MODULE: moduledrawers.SquareModuleDrawer(),
+        QRCodeDrawers.VERTICAL_BARS: moduledrawers.VerticalBarsDrawer(),
+    }.get(drawer, moduledrawers.SquareModuleDrawer())
+
+
+def mask_factory(
+    mask: QRCodeMasks | None = None,
+    background_color: str | None = None,
+    primary_color: str | None = None,
+    secondary_color: str | None = None,
+) -> colormasks.QRColorMask:
+    return {
+        QRCodeMasks.HORIZONTAL_GRADIENT: partial(
+            colormasks.HorizontalGradiantColorMask,
+            back_color=background_color,
+            left_color=primary_color,
+            right_color=secondary_color,
+        ),
+        QRCodeMasks.IMAGE_COLOR: partial(
+            colormasks.ImageColorMask, back_color=background_color
+        ),
+        QRCodeMasks.RADIAL_GRADIENT: partial(
+            colormasks.RadialGradiantColorMask,
+            back_color=background_color,
+            center_color=primary_color,
+            edge_color=secondary_color,
+        ),
+        QRCodeMasks.SOLID_FILL: partial(
+            colormasks.SolidFillColorMask,
+            back_color=background_color,
+            front_color=primary_color,
+        ),
+        QRCodeMasks.SQUARE_GRADIENT: partial(
+            colormasks.SquareGradiantColorMask,
+            back_color=background_color,
+            center_color=primary_color,
+            edge_color=secondary_color,
+        ),
+        QRCodeMasks.VERTICAL_GRADIENT: partial(
+            colormasks.VerticalGradiantColorMask,
+            back_color=background_color,
+            top_color=primary_color,
+            bottom_color=secondary_color,
+        ),
+    }.get(
+        mask,
+        partial(
+            colormasks.SolidFillColorMask,
+            back_color=background_color,
+            front_color=primary_color,
+        ),
+    )()
+
+
+def make_qr_code(
+    data: str,
+    drawer: QRCodeDrawers | None = None,
+    mask: QRCodeMasks | None = None,
+    background_color: str | None = None,
+    primary_color: str | None = None,
+    secondary_color: str | None = None,
+):
+    primary_color = (
+        RGBColor.from_hex_color(primary_color) if primary_color else RGBColor((0, 0, 0))
+    )
+    secondary_color = (
+        RGBColor.from_hex_color(secondary_color)
+        if secondary_color
+        else RGBColor((125, 125, 125))
+    )
+    background_color = (
+        RGBColor.from_hex_color(background_color)
+        if background_color
+        else RGBColor((255, 255, 255))
+    )
+    mask_instance = mask_factory(mask, background_color, primary_color, secondary_color)
+    qr = qrcode.QRCode(
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        image_factory=StyledPilImage,
+    )
+    qr.add_data(f"{data}")
+    img = qr.make_image(
+        module_drawer=map_drawers(drawer),
+        color_mask=mask_instance,
+    )
+    img_buffer = io.BytesIO()
+    img.save(img_buffer, format="PNG")
+    img_buffer.seek(0)
+    return img_buffer
+
+
 @app.get("/qr", response_model=ListQRCodeAPIResponse)
 async def fetch_qr_codes(request: Request, page: int = 1, page_size: int = 10):
     results = list(collection.find().skip((page - 1) * page_size).limit(page_size))
     return ListQRCodeAPIResponse(
-        items=[
-            GetQRCodeAPIResponse(**result) for result in results
-        ],
+        items=[GetQRCodeAPIResponse(**result) for result in results],
         page=page,
         page_size=page_size,
+        next_page=page + 1 if len(results) == page_size else None,
+    )
+
+
+@app.get("/qr-preview", response_model=GetPreviewAPIResponse)
+def get_qr_code_preview(
+    request: Request,
+    drawer: QRCodeDrawers | None = None,
+    mask: QRCodeMasks | None = None,
+    background_color: str | None = None,
+    primary_color: str | None = None,
+    secondary_color: str | None = None,
+):
+    img_buffer = make_qr_code(
+        request.base_url, drawer, mask, background_color, primary_color, secondary_color
+    )
+    return GetPreviewAPIResponse(
+        image=base64.b64encode(img_buffer.getvalue()).decode("utf-8"),
     )
 
 
@@ -145,14 +334,14 @@ async def fetch_qr_codes(request: Request, page: int = 1, page_size: int = 10):
 async def create_qr_code(payload: CreateQRCodeAPIRequest, request: Request):
     name = payload.name or extract_domain_from_url(payload.target_url)
     img_name = hash_string(payload.target_url)
-    qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L)
-    qr.add_data(f"{request.base_url}scan/{img_name}")
-    img = qr.make_image(
-        image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer()
+    img_buffer = make_qr_code(
+        f"{request.base_url}scan/{img_name}",
+        payload.drawer,
+        payload.mask,
+        payload.background_color,
+        payload.primary_color,
+        payload.secondary_color,
     )
-    img_buffer = io.BytesIO()
-    img.save(img_buffer, "PNG")
-    img_buffer.seek(0)
     qr_db_instance = QRCodeEntryDB(
         name=name,
         target_url=payload.target_url,
--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,51 @@
+htmx.defineExtension('qr-form-controller', {
+  onEvent: function (name, evt) {
+      if (name === "htmx:configRequest" && evt.detail.elt.id === "new-qr-form") {
+        evt.detail.headers['Content-Type'] = "application/json";
+        evt.detail.elt.elements[7].classList.add("disabled");
+        evt.detail.elt.lastElementChild.classList.remove("d-none");
+        
+      }
+
+      if (name === "htmx:afterRequest" && evt.detail.elt.id === "new-qr-form") {
+        animateCSS('#form-row', 'fadeOut').then((message) => {
+          evt.target.lastElementChild.classList.add("d-none");
+          evt.target.parentElement.parentElement.classList.add("d-none");
+          console.log(evt.target.parentElement.parentElement);
+          evt.target[7].classList.remove("disabled");
+          animateCSS('#new-qr', 'fadeIn')
+          document.querySelector("#new-qr").classList.remove("d-none");
+          document.querySelector("#new-qr-form").reset();
+        });
+      }
+  },
+  
+  encodeParameters : function(xhr, parameters, elt) {
+      xhr.overrideMimeType('text/json');
+      console.log(parameters);
+      let parsedParameters = parseDotKeysAsNestedObjects(parameters);
+      return (JSON.stringify(parsedParameters));
+  }
+});
+
+function parseDotKeysAsNestedObjects(obj) {
+  const result = {};
+  Object.keys(obj).forEach((key) => {
+    const value = obj[key];
+    const parts = key.split('.');
+    let current = result;
+    for (let i = 0; i < parts.length - 1; i++) {
+      const part = parts[i];
+      if (!current[part]) {
+        current[part] = {};
+      }
+      current = current[part];
+    }
+    current[parts[parts.length - 1]] = value;
+  });
+  return result;
+}
+
 function padTo2Digits(num) {
   return num.toString().padStart(2, '0');
 }
@@ -27,3 +75,78 @@ function getFavicon(url, size) {
 
 nunjucksEnv.addFilter('shortenUrl', shortenUrl);
 nunjucksEnv.addFilter('getFavicon', getFavicon);
+
+htmx.logger = function(elt, event, data) {
+  console.log(event, elt, data);
+}
+
+
+htmx.defineExtension('qr-preview', {
+  onEvent: function (name, evt) {
+    if (name === "htmx:configRequest") {
+      let elt = evt.detail.elt.closest("#new-qr-form");
+      let previewParams = {};
+      let inputs = [];
+      findTagInChildrenRecursive(elt, "input", inputs);
+      findTagInChildrenRecursive(elt, "select", inputs);
+      for (key in inputs) {
+        if (inputs[key].name) {
+          previewParams[inputs[key].name] = inputs[key].value;
+        }
+      }
+      evt.detail.path += "?" + objectToUrlParamsRecursive(previewParams);
+      console.log(evt.detail.path);
+    }
+  },
+});
+
+function findTagInChildrenRecursive(elt, tag, result) {
+  if (elt.tagName.toLowerCase() === tag) {
+    result.push(elt);
+  }
+  for (var i = 0; i < elt.children.length; i++) {
+    findTagInChildrenRecursive(elt.children[i], tag, result);
+  }
+}
+
+function objectToUrlParamsRecursive(obj, prefix) {
+  var str = [];
+  for(var p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
+      str.push((v !== null && typeof v === "object") ?
+        objectToUrlParamsRecursive(v, k) :
+        encodeURIComponent(k) + "=" + encodeURIComponent(v));
+    }
+  }
+  return str.join("&");
+}
+
+const animateCSS = (element, animation, prefix = 'animate__') =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.querySelector(element);
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve('Animation ended');
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd, {once: true});
+  });
+
+
+document.addEventListener("click", function(event) {
+  if (event.target.id === "create-new-qr-btn") {
+    animateCSS('#new-qr', 'fadeOut');
+    let new_qr = document.querySelector("#new-qr");
+    new_qr.removeChild(new_qr.lastElementChild);
+    animateCSS("#form-row", "fadeIn");
+    document.querySelector("#form-row").classList.remove("d-none");
+  }
+});
